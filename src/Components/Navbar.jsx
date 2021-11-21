b202f31559be566b087383bc15920206244016a7
Ionu <itsbratu@outlook.com>
2021-11-21 13:01:38 +0200

    navbar styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,11 +3,27 @@ import React from 'react'
 const Navbar = () => {
     return(
         <div class = "main-nav ">
-            <ul>
-                <li><a href = "#">Home</a></li>
-                <li><a href = "#">About</a></li>
-                <li><a href = "#">Services</a></li>
-                <li><a href = "#">Contact</a></li>
+            <ul className = "grid grid-cols-4 gap-5 p-0 list-none">
+                <li class = "bg-primary block no-underline p-3 text-center text-dark uppercase text-lg shadow-lg hover:bg-dark hover:text-light">
+                    <a href = "#">
+                        Home
+                    </a>
+                </li>
+                <li class = "bg-primary block no-underline p-3 text-center text-dark uppercase text-lg shadow-lg hover:bg-dark hover:text-light">
+                    <a href = "#">
+                        About
+                    </a>
+                </li>
+                <li class = "bg-primary block no-underline p-3 text-center text-dark uppercase text-lg shadow-lg hover:bg-dark hover:text-light">
+                    <a href = "#">
+                        Services
+                    </a>
+                </li>
+                <li class = "bg-primary block no-underline p-3 text-center text-dark uppercase text-lg shadow-lg hover:bg-dark hover:text-light">
+                    <a href = "#">
+                        Contact
+                    </a>
+                </li>
             </ul>
         </div>
     )
